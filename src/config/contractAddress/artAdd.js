6e02f36fbc02b8cce03a3cd585d06961325fc6ef
jawadhaxan <jawadhaxan@outlook.com>
2021-05-06 00:42:16 +0500

    mainnet

--- src/config/contractAddress/artAdd.js
+++ src/config/contractAddress/artAdd.js
@@ -4,7 +4,7 @@ let add = "";
 if (networkType === "testnet") {
   add = "0x68091fcE8d0E0f39DDb118F5753eC1316270F064"; // test net
 } else if (networkType === "mainnet") {
-  add = ""; // main net
+  add = "0x7216f56167a607E406fAAC4585338e8232B5EA03"; // main net
 }
 
 export default add;
--- src/config/contractAddress/ico.js
+++ src/config/contractAddress/ico.js
@@ -4,7 +4,7 @@ let add = "";
 if (networkType === "testnet") {
   add = "0xde3CE1d9b11A202851DbF2DB8238B0DD1F0ad31E"; // test net
 } else if (networkType === "mainnet") {
-  add = ""; // main net
+  add = "0x48A13a1e17A887374375681bEB16e79fAF822886"; // main net
 }
 
 export default add;
